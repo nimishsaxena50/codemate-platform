06210d498e7b8de46433d096c049656f760b68be
nimishsaxena50 <saxenanimish473@gmail.com>
2025-09-15 22:27:14 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,12 @@ connect();
 
 const app = express();
 
-app.use(cors());
-app.use(morgan('dev'));
+
+app.use(cors({
+  origin: "http://localhost:5173", // yaha apne frontend ka URL daalna
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true
+}));app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
